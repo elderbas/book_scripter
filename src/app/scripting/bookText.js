269ca186278fcf0bcf5a06b8097083ebfe2846af
bschermerhorn <bschermerhorn@bigsquid.com>
2016-07-13 01:22:10 -0500

    jquery, jquery selection, lodash

--- src/app/scripting/bookText.js
+++ src/app/scripting/bookText.js
@@ -2,15 +2,16 @@ angular.module('app').directive('bookText', function () {
   return {
     link,
     templateUrl: 'app/scripting/bookText.html',
-    scope: {
-
-    },
+    scope: {},
     restrict: 'E'
   };
 
+  // http://unicode-table.com/en/201C/
   function link (scope, element) {
     element.on('mouseup', function (e) {
-      console.log(e);
+      console.log($.selection().charCodeAt(0));
+
+      console.log(element.text());
     });
   }
 });
